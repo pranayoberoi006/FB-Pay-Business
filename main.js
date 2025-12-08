81fa51130f033c8141642bd68174028ae243d1a6
pranayoberoi006 <oberoipranay0@gmail.com>
2025-12-09 02:00:52 +0530

    Initial frontend upload

---
+++ main.js
@@ -0,0 +1,42 @@
+document.getElementById("userForm").onsubmit = async function(e){
+    e.preventDefault();
+
+    const userData = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+        amount: document.getElementById("amount").value
+    };
+
+    let order = await fetch("http://localhost:5000/create-order", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(userData)
+    });
+
+    order = await order.json();
+
+    var options = {
+        "key": "YOUR_KEY_ID",
+        "amount": order.amount,
+        "currency": "INR",
+        "name": "FB Pay Business",
+        "description": "Payment",
+        "order_id": order.id,
+
+        "handler": async function (response){
+            await fetch("http://localhost:5000/payment-success", {
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify({
+                    payment: response,
+                    user: userData
+                })
+            });
+            alert("Payment Successful! Receipt sent to your Email and Phone.");
+        }
+    };
+
+    var rzp1 = new Razorpay(options);
+    rzp1.open();
+};
